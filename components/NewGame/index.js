6b451f6056fddf48da890f88a578f46403cacb0f
Ross Salge <rossnsalge@gmail.com>
2018-03-12 21:19:05 -0500

    Adds ability to enter players, change player order, remove player. Adds GameInProgess component not tied into Start Game button yet

--- components/NewGame/index.js
+++ components/NewGame/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { StyleSheet, Input, View, Text } from "react-native";
-import { Header, FormInput, Button, InputGroup } from "react-native-elements";
+import {
+  Icon,
+  Header,
+  FormInput,
+  Button,
+  InputGroup,
+  FormValidationMessage
+} from "react-native-elements";
+import PlayerList from "./PlayerList";
 
 export default class NewGame extends Component {
   state = {
     addPlayerName: "",
     addPlayerColor: "",
-    players: ["Ross", "Pete"],
+    players: [],
     victoryPoints: 10,
-    playerName: ""
+    playerName: "",
+    error: null
   };
 
   handleInputChange = e => {
@@ -20,6 +29,51 @@ export default class NewGame extends Component {
   };
   updatePlayerName = e => {
     this.setState({ playerName: e.target.value });
+  };
+  addPlayer = () => {
+    let { players, playerName } = this.state;
+    let error = null;
+    playerName = playerName.trim().toUpperCase();
+    if (!players.includes(playerName)) {
+      players.push(playerName);
+    } else {
+      error = "Player name already exists";
+    }
+    this.setState({
+      players,
+      playerName: "",
+      error
+    });
+  };
+  movePlayerUp = player => {
+    let { players } = this.state;
+    let i = players.indexOf(player);
+    console.log("index: ", i);
+    if (i > 0) {
+      console.log("made it in");
+      [players[i], players[i - 1]] = [players[i - 1], players[i]];
+    }
+    this.setState({ players });
+    console.log(players);
+  };
+  movePlayerDown = player => {
+    let { players } = this.state;
+    let i = players.indexOf(player);
+    console.log("index: ", i);
+    if (i < players.length - 1) {
+      console.log("made it in");
+      [players[i], players[i + 1]] = [players[i + 1], players[i]];
+    }
+    this.setState({ players });
+    console.log(players);
+  };
+  deletePlayer = player => {
+    let { players } = this.state;
+    let i = players.indexOf(player);
+    players.splice(i, 1);
+    this.setState({
+      players
+    });
   };
 
   render() {
@@ -34,13 +88,35 @@ export default class NewGame extends Component {
             style: { color: "#fff" }
           }}
         />
-        <FormInput
-          placeholder="BASIC INPUT"
-          value={this.state.playerName}
-          onChangeText={text => this.setState({ playerName: text })}
+        <View style={{ display: "flex", flexDirection: "row" }}>
+          <FormInput
+            containerStyle={{ width: "60%" }}
+            placeholder="BASIC INPUT"
+            value={this.state.playerName}
+            onChangeText={text => this.setState({ playerName: text })}
+          />
+          {/* <Icon raised name="add" onPress={this.addPlayer} /> */}
+          <Button
+            disabled={this.state.playerName.length === 0}
+            borderRadius="100%"
+            buttonStyle={{ width: 60 }}
+            containerViewStyle={{ borderRadius: "100%", width: 60 }}
+            icon={{ name: "add" }}
+            onPress={this.addPlayer}
+            raised
+            rounded
+          />
+        </View>
+        {this.state.error && (
+          <FormValidationMessage>{this.state.error}</FormValidationMessage>
+        )}
+        <PlayerList
+          players={this.state.players}
+          movePlayerUp={this.movePlayerUp}
+          movePlayerDown={this.movePlayerDown}
+          deletePlayer={this.deletePlayer}
         />
-        <Button onClick={this.addPlayer} title="Add Player" />
-        {this.state.players.map(player => <Text> {player} </Text>)}
+        <Button onPress={this.startGame} title="Start Game" />
       </View>
     );
   }
